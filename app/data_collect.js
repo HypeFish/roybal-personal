60b13c67f07215fbf1258654c8dd7438fc52c20a
HypeFish <99514952+HypeFish@users.noreply.github.com>
2023-09-05 16:30:08 -0400

    7 days

--- app/data_collect.js
+++ app/data_collect.js
@@ -1,11 +1,15 @@
 // Description: This file is used to collect data from Fitbit API
 
+// Fitbit API client ID
+const client_id = "23RCXD"
+
+// Fitbit API access token
 const access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM1JDWEQiLCJzdWIiOiJCUFM1V1EiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJlY2cgcnNldCByb3h5IHJudXQgcnBybyByc2xlIHJjZiByYWN0IHJsb2MgcnJlcyByd2VpIHJociBydGVtIiwiZXhwIjoxNjkzOTcwNjE1LCJpYXQiOjE2OTM5NDE4MTV9.Ws-9kDltFISGUvffNICWR02t8dIswZyGvIawz2N9lBQ"
 
 // Fetch data from the Fitbit API
 document.getElementById("generateCsvButton").addEventListener("click", () => {
     // Fetch data from the Fitbit API
-    fetch('https://api.fitbit.com/1/user/-/activities/steps/date/today/1y.json', {
+    fetch('https://api.fitbit.com/1/user/-/activities/steps/date/today/7d.json', {
         method: "GET",
         headers: { "Authorization": "Bearer " + access_token }
     })
@@ -33,5 +37,3 @@ document.getElementById("generateCsvButton").addEventListener("click", () => {
         })
         .catch(error => console.error(error));
 });
-
-//convert steps json to csv that is saved to /data/steps.csv
