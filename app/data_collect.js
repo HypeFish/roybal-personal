fa90023d30be678553b52f3ebe85958a8394eec6
HypeFish <99514952+HypeFish@users.noreply.github.com>
2023-09-05 16:09:26 -0400

    owo?

--- app/data_collect.js
+++
@@ -1,8 +0,0 @@
-// Description: This file is used to collect data from Fitbit API
-
-fetch('https://api.fitbit.com/1/user/-/activities/steps/date/today/1y.json', {
-    method: "GET",
-    headers: {"Authorization": "Bearer " + access_token}
-})
-.then(response => response.json()) 
-.then(json => console.log(json)); 
