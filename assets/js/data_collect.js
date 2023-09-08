a3b6892cb1b585f998c1e044170bf1aca3ad8723
HypeFish <99514952+HypeFish@users.noreply.github.com>
2023-09-08 10:12:31 -0400

    owo???

--- assets/js/data_collect.js
+++ assets/js/data_collect.js
@@ -2,27 +2,34 @@
 
 // Fitbit API access token and refresh token
 let access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM1JDWEQiLCJzdWIiOiJCUFM1V1EiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJlY2cgcnNldCByb3h5IHJwcm8gcm51dCByc2xlIHJjZiByYWN0IHJyZXMgcmxvYyByd2VpIHJociBydGVtIiwiZXhwIjoxNjk0MTM4NDcxLCJpYXQiOjE2OTQxMDk2NzF9.EqiloL6Xy8UMsdYJ2e24_jDc-a_tzmWVuAGH3rPJBas"; // Your initial access token
-const refresh_token = "37a61a939b3aa0b7d308a571283a658987e93622e8ea24dde02683ec95974505";
+const refresh_token = "2a91b5e92112edd0020d268310ee435e0f37c6044c51db168451555cdb557e0c";
 
 function refreshAccessToken() {
     return fetch('https://api.fitbit.com/oauth2/token', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic MjNSQ1hEOmYxZDRiZmYzZmNhZmEwM2UxYzkyMDA5NDEyMjI0YjI2' // Replace with your client ID and secret
+            'Authorization': 'Basic MjNSQ1hEOmYxZDRiZmYzZmNhZmEwM2UxYzkyMDA5NDEyMjI0YjI2'
         },
         body: `grant_type=refresh_token&refresh_token=${refresh_token}`
     })
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
         .then(data => {
             if (data.access_token) {
-                access_token = data.access_token; // Update the access token
+                access_token = data.access_token;
                 return access_token;
             } else {
                 throw new Error('Unable to refresh access token');
             }
-        });
+        })
+        .catch(error => console.error('Error:', error));
 }
+
 
 //Function to get the activity data from the Fitbit API 
 //TO READ THIS GO TO INSPECT AND THEN NETWORK AFTER PRESSING THE BUTTON CAUSE THAT MAKES THE API
