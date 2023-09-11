b8e8792b51c2e1575928feb7f7216cb93219f055
HypeFish <99514952+HypeFish@users.noreply.github.com>
2023-09-11 10:52:48 -0400

    help im dying

--- assets/js/data_collect.js
+++ assets/js/data_collect.js
@@ -9,10 +9,12 @@ fetch('/api/tokens')
     .then(data => {
         access_token = data.access_token;
         refresh_token = data.refresh_token;
-        
+
         // Use the tokens as needed
     })
     .catch(error => console.error('Error:', error));
+
+let apiData = []; // Store the data from API calls
 
 function refreshAccessToken() {
     return fetch('https://api.fitbit.com/oauth2/token', {
@@ -43,49 +45,55 @@ function refreshAccessToken() {
         .catch(error => console.error('Error:', error));
 }
 
-// Function to make the API call with the current access token
 function makeApiCall() {
-    console.log('Making API call with access token:', access_token + " " + refresh_token)
-    fetch('https://api.fitbit.com/1/user/-/activities/steps/date/today/7d.json', {
+    console.log('Making API call with access token:', access_token + " " + refresh_token);
+    return fetch('https://api.fitbit.com/1/user/-/activities/steps/date/today/7d.json', {
         method: "GET",
         headers: { "Authorization": "Bearer " + access_token }
     })
-    .then(response => {
-        if (response.status === 401) {
-            // If the token has expired, refresh it and retry the request
-            return refreshAccessToken().then(() => makeApiCall());
-        }
-        return response.json();
-    })
-    .then(json => {
-        // Create a CSV string with a title row
-        let csvData = "Date,Step Counter\n";
-        csvData += json["activities-steps"].map(item => {
-            return `${item.dateTime},${item.value}`;
-        }).join('\n');
-
-        // Create a Blob with the CSV data
-        const blob = new Blob([csvData], { type: 'text/csv' });
-
-        // Create a download link for the CSV file
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'steps.csv';
-
-        // Trigger the download
-        a.click();
-
-        // Release the URL object
-        window.URL.revokeObjectURL(url);
-    })
-    .catch(error => console.error(error));
+        .then(response => {
+            if (response.status === 401) {
+                // If the token has expired, refresh it and retry the request
+                return refreshAccessToken().then(() => makeApiCall());
+            }
+            return response.json();
+        })
+        .then(json => {
+            apiData.push(json); // Store the data
+        })
+        .catch(error => console.error(error));
 }
 
-// Function to handle the button click event
+function generateCSV() {
+    // Consolidate data and generate CSV here
+    let csvData = "Date,Step Counter\n";
+    apiData.forEach(apiResponse => {
+        csvData += apiResponse["activities-steps"].map(item => {
+            return `${item.dateTime},${item.value}`;
+        }).join('\n');
+    });
+
+    // Create a Blob with the CSV data
+    const blob = new Blob([csvData], { type: 'text/csv' });
+
+    // Create a download link for the CSV file
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'steps.csv';
+
+    // Trigger the download
+    a.click();
+
+    // Release the URL object
+    window.URL.revokeObjectURL(url);
+}
+
 function handleButtonClick() {
     // Fetch data from the Fitbit API
-    makeApiCall();
+    makeApiCall().then(() => {
+        generateCSV(); // Generate CSV after API calls are complete
+    });
 }
 
 // Attach the click event handler to the button
--- server.js
+++ server.js
@@ -11,26 +11,36 @@ let access_token;
 let refresh_token;
 
 async function fetchInitialTokens() {
-    const response = await fetch('https://api.fitbit.com/oauth2/token', {
-        method: 'POST',
-        headers: {
-            'Authorization': `Basic ${Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')}`,
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: `client_id=${process.env.CLIENT_ID}&grant_type=authorization_code&redirect_uri=https%3A%2F%2Froybal.vercel.app%2F&code=a638280fe30ef30fdf3d0c079bf75b821b634270&code_verifier=6g475k1l2t4e6y3m4g3i2i700v6e45536v0a0i5q2w6258003r0h0j6b473j2l171o4l4s2r2k0b11473i443h0k0d692u2y715n5h3l521t3z4c031k404v4q2a5v61`
-    });
+    let validTokens = false;
+    while (!validTokens) {
+        const response = await fetch('https://api.fitbit.com/oauth2/token', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Basic ${Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')}`,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: `client_id=${process.env.CLIENT_ID}&grant_type=authorization_code&redirect_uri=https%3A%2F%2Froybal.vercel.app%2F&code=46603952c7b2c753eea80e59354ce16412631d0b&code_verifier=6n072h360k124j1t38313o0z2p61706l612e5s5c5v3j2a3v552j3z1g2e220e0m4k3o1q5q3h0o053i363n6t1n3h085i5v6x435u0o236m3t3i4d1u361t3q364m15`
+        });
 
-    const data = await response.json();
-    access_token = data.access_token;
-    refresh_token = data.refresh_token;
-
-    process.env.ACCESS_TOKEN = access_token;
-    process.env.REFRESH_TOKEN = refresh_token;
-
-    console.log('Access Token:', process.env.ACCESS_TOKEN);
-    console.log('Refresh Token:', process.env.REFRESH_TOKEN);
-    console.log('Fitbit Response:', data);
+        const data = await response.json();
+        
+        if (data.access_token && data.refresh_token) {
+            validTokens = true;
+            access_token = data.access_token;
+            refresh_token = data.refresh_token;
+            process.env.ACCESS_TOKEN = access_token;
+            process.env.REFRESH_TOKEN = refresh_token;
+            console.log('Access Token:', process.env.ACCESS_TOKEN);
+            console.log('Refresh Token:', process.env.REFRESH_TOKEN);
+            console.log('Fitbit Response:', data);
+        } else {
+            console.log('Invalid tokens, fetching new pair...');
+        }
+    }
 }
+
+fetchInitialTokens();
+
 
 
 // Call the function to fetch tokens
