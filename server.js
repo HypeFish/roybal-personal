ee02d15b099ff49d34b5a692b45a9aa7c492c1fc
HypeFish <99514952+HypeFish@users.noreply.github.com>
2023-09-12 16:11:37 -0400

    we are so back

--- server.js
+++ server.js
@@ -11,62 +11,56 @@ const app = express();
 let access_token;
 let refresh_token;
 
-async function fetchInitialTokens() {
-    let validTokens = false;
-    let retryCount = 0;
+// async function fetchInitialTokens() {
+//     let validTokens = false;
+//     let retryCount = 0;
     
-    while (!validTokens && retryCount < MAX_RETRY_COUNT) {
-        try {
-            const response = await fetch('https://api.fitbit.com/oauth2/token', {
-                method: 'POST',
-                headers: {
-                    'Authorization': `Basic ${Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')}`,
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: `client_id=${process.env.CLIENT_ID}&grant_type=authorization_code&redirect_uri=${process.env.REDIRECT_URI}&code=${process.env.CODE}&code_verifier=${process.env.PKCE_CODE_VERIFIER}`
-            });
+//     while (!validTokens && retryCount < MAX_RETRY_COUNT) {
+//         try {
+//             const response = await fetch('https://api.fitbit.com/oauth2/token', {
+//                 method: 'POST',
+//                 headers: {
+//                     'Authorization': `Basic ${Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')}`,
+//                     'Content-Type': 'application/x-www-form-urlencoded'
+//                 },
+//                 body: `client_id=${process.env.CLIENT_ID}&grant_type=authorization_code&redirect_uri=${process.env.REDIRECT_URI}&code=${process.env.CODE}&code_verifier=${process.env.PKCE_CODE_VERIFIER}`
+//             });
 
-            const data = await response.json();
+//             const data = await response.json();
 
-            if (data.access_token && data.refresh_token) {
-                validTokens = true;
-                access_token = data.access_token;
-                refresh_token = data.refresh_token;
-                process.env.ACCESS_TOKEN = access_token;
-                process.env.REFRESH_TOKEN = refresh_token;
+//             if (data.access_token && data.refresh_token) {
+//                 validTokens = true;
+//                 access_token = data.access_token;
+//                 refresh_token = data.refresh_token;
+//                 process.env.ACCESS_TOKEN = access_token;
+//                 process.env.REFRESH_TOKEN = refresh_token;
 
-                console.log('Access Token:', process.env.ACCESS_TOKEN);
-                console.log('Refresh Token:', process.env.REFRESH_TOKEN);
-                console.log('Fitbit Response:', data);
-            } else {
-                console.log('Invalid tokens, fetching new pair...');
-            }
-        } catch (error) {
-            console.error('Error fetching tokens:', error);
-        } finally {
-            retryCount++;
-        }
-    }
-}
+//                 console.log('Access Token:', process.env.ACCESS_TOKEN);
+//                 console.log('Refresh Token:', process.env.REFRESH_TOKEN);
+//                 console.log('Fitbit Response:', data);
+//             } else {
+//                 console.log('Invalid tokens, fetching new pair...');
+//             }
+//         } catch (error) {
+//             console.error('Error fetching tokens:', error);
+//         } finally {
+//             retryCount++;
+//         }
+//     }
+// }
 
-const MAX_RETRY_COUNT = 3; // Define a maximum number of retries
+// const MAX_RETRY_COUNT = 3; // Define a maximum number of retries
 
-fetchInitialTokens();
+// fetchInitialTokens();
 
 // Serve static files from the 'assets' directory
 const publicPath = '/assets'; // Set the correct public path
 app.use(publicPath, express.static(path.join(__dirname, 'assets')));
 
 
-app.get('/api/tokens', (req, res) => {
-    res.json({
-        access_token: process.env.ACCESS_TOKEN,
-        refresh_token: process.env.REFRESH_TOKEN,
-    });
-});
-
 // Add a new route for the authorization callback
 app.get('/auth/callback', async (req, res) => {
+    console.log('Reached /auth/callback');
     res.sendFile(path.join(__dirname, 'index.html'));
     
     const authorizationCode = req.query.code; // Extract the authorization code from the URL
@@ -94,6 +88,13 @@ app.get('/auth/callback', async (req, res) => {
 
     // Redirect the user to a new page or send a response as needed
     res.send('Authorization complete. You can now access Fitbit data.');
+});
+
+app.get('/api/tokens', (req, res) => {
+    res.json({
+        access_token: process.env.ACCESS_TOKEN,
+        refresh_token: process.env.REFRESH_TOKEN,
+    });
 });
 
 //Serve the index page
