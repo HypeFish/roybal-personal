797511762e6df7122578fb10f6894db91221af50
HypeFish <99514952+HypeFish@users.noreply.github.com>
2023-09-12 15:51:12 -0400

    what

--- server.js
+++ server.js
@@ -4,46 +4,53 @@ const path = require('path');
 const fetch = require('node-fetch');
 const dotenv = require('dotenv');
 dotenv.config({ path: 'env/user1.env' }); // This will read the env/user1.env file and set the environment variables
+dotenv.config({ path: 'env/user2.env' }); // This will read the env/user2.env file and set the environment variables
 
 const app = express();
 
 let access_token;
 let refresh_token;
-let clientId = process.env.CLIENT_ID;
-let clientSecret = process.env.CLIENT_SECRET;
-let redirectUri = process.env.REDIRECT_URI;
-
 
 async function fetchInitialTokens() {
     let validTokens = false;
-    while (!validTokens) {
-        const response = await fetch('https://api.fitbit.com/oauth2/token', {
-            method: 'POST',
-            headers: {
-                'Authorization': `Basic ${Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')}`,
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: `client_id=${process.env.CLIENT_ID}&grant_type=authorization_code&redirect_uri=https%3A%2F%2Froybal.vercel.app%2F&code=46603952c7b2c753eea80e59354ce16412631d0b&code_verifier=6n072h360k124j1t38313o0z2p61706l612e5s5c5v3j2a3v552j3z1g2e220e0m4k3o1q5q3h0o053i363n6t1n3h085i5v6x435u0o236m3t3i4d1u361t3q364m15`
-        });
+    let retryCount = 0;
+    
+    while (!validTokens && retryCount < MAX_RETRY_COUNT) {
+        try {
+            const response = await fetch('https://api.fitbit.com/oauth2/token', {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Basic ${Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')}`,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: `client_id=${process.env.CLIENT_ID}&grant_type=authorization_code&redirect_uri=${process.env.REDIRECT_URI}&code=${process.env.CODE}&code_verifier=${process.env.PKCE_CODE_VERIFIER}`
+            });
 
-        const data = await response.json();
-        
-        if (data.access_token && data.refresh_token) {
-            validTokens = true;
-            access_token = data.access_token;
-            refresh_token = data.refresh_token;
-            process.env.ACCESS_TOKEN = access_token;
-            process.env.REFRESH_TOKEN = refresh_token;
-            console.log('Access Token:', process.env.ACCESS_TOKEN);
-            console.log('Refresh Token:', process.env.REFRESH_TOKEN);
-            console.log('Fitbit Response:', data);
-        } else {
-            console.log('Invalid tokens, fetching new pair...');
+            const data = await response.json();
+
+            if (data.access_token && data.refresh_token) {
+                validTokens = true;
+                access_token = data.access_token;
+                refresh_token = data.refresh_token;
+                process.env.ACCESS_TOKEN = access_token;
+                process.env.REFRESH_TOKEN = refresh_token;
+
+                console.log('Access Token:', process.env.ACCESS_TOKEN);
+                console.log('Refresh Token:', process.env.REFRESH_TOKEN);
+                console.log('Fitbit Response:', data);
+            } else {
+                console.log('Invalid tokens, fetching new pair...');
+            }
+        } catch (error) {
+            console.error('Error fetching tokens:', error);
+        } finally {
+            retryCount++;
         }
     }
 }
 
-// Call the function to fetch tokens
+const MAX_RETRY_COUNT = 3; // Define a maximum number of retries
+
 fetchInitialTokens();
 
 // Serve static files from the 'assets' directory
@@ -54,7 +61,7 @@ app.use(publicPath, express.static(path.join(__dirname, 'assets')));
 app.get('/api/tokens', (req, res) => {
     res.json({
         access_token: process.env.ACCESS_TOKEN,
-        refresh_token: process.env.REFRESH_TOKEN
+        refresh_token: process.env.REFRESH_TOKEN,
     });
 });
 
@@ -66,14 +73,38 @@ app.get('/', (req, res) => {
     });
 });
 
-// Serve the callback page
-app.get('/callback', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 // Serve the error page
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
+
+// Add a new route for the authorization callback
+app.get('/auth/callback', async (req, res) => {
+    const authorizationCode = req.query.code; // Extract the authorization code from the URL
+
+    // Use the authorization code to obtain access token
+    const response = await fetch('https://api.fitbit.com/oauth2/token', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Basic ${Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `client_id=${process.env.CLIENT_ID}&grant_type=authorization_code&redirect_uri=${process.env.REDIRECT_URI}&code=${authorizationCode}&code_verifier=${process.env.PKCE_CODE_VERIFIER}`
+    });
+
+    const data = await response.json();
+    access_token = data.access_token;
+    refresh_token = data.refresh_token;
+
+    process.env.ACCESS_TOKEN = access_token;
+    process.env.REFRESH_TOKEN = refresh_token;
+
+    console.log('Access Token:', process.env.ACCESS_TOKEN);
+    console.log('Refresh Token:', process.env.REFRESH_TOKEN);
+    console.log('Fitbit Response:', data);
+
+    // Redirect the user to a new page or send a response as needed
+    res.send('Authorization complete. You can now access Fitbit data.');
 });
 
 const port = process.env.PORT || 3000;
